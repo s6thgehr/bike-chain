078422d0ff6de50dd472a282dd02fe8909a4f16d
Thomas Gehrmann <s6thgehr@gmail.com>
2022-12-24 23:15:07 +0100

    Upgrade zustand package

--- src/stores/useNotificationStore.tsx
+++ src/stores/useNotificationStore.tsx
@@ -1,19 +1,19 @@
-import create, { State } from "zustand";
+import create from "zustand";
 import produce from "immer";
 
-interface NotificationStore extends State {
-  notifications: Array<{
-    type: string
-    message: string
-    description?: string
-    txid?: string
-  }>
-  set: (x: any) => void
+interface NotificationStore {
+    notifications: Array<{
+        type: string;
+        message: string;
+        description?: string;
+        txid?: string;
+    }>;
+    set: (x: any) => void;
 }
 
-const useNotificationStore = create<NotificationStore>((set, _get) => ({
-  notifications: [],
-  set: (fn) => set(produce(fn)),
-}))
+const useNotificationStore = create<NotificationStore>()((set, _get) => ({
+    notifications: [],
+    set: (fn) => set(produce(fn)),
+}));
 
-export default useNotificationStore
+export default useNotificationStore;
